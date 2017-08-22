406187555a61a701e40a4afdb93c14a298a3f198
Kent Saeteurn <kentsaeteurn@gmail.com>
2017-08-22 12:36:38 -0700

    add empty results area when user deletes search string

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
 var api, userInput;
-function myFunction() {
-  $("#results").empty();
-  userInput = $("input").val();
-  api = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + userInput + "&format=json";
-  $.ajax({
-    url: api,
-    dataType: "jsonp",
-    // jsonp: "jsoncallback",
-    success:
-    function processJson(json){
-      for (var i = 0; i < json.query.search.length; i++){
-        var title = json.query.search[i].title;
-        var snippet = json.query.search[i].snippet;
-        var urlLink = "https://en.wikipedia.org/?curid=" + json.query.search[i].pageid;
-        $("#results").append(
-          "<a target='_blank' href=" + urlLink + ">" +
-            "<div>" +
-              "<span id='title'>" + title + "</span><br>" +
-              "<span id='snippet'>" + snippet + "</span>" +
-            "</div>" +
-          "</a>"
-        );
-      }//end for
-    },//end processJson
-    error:
-    function(XMLHttpRequest, textStatus, errorThrown) {
-      alert("Status: " + textStatus); alert("Error: " + errorThrown);
-    }
-  });//end .ajax
-};//end myFunction()
+$("input").on("keyup", function(event) {
+  if (event.which == 13) {
+    $("#results").empty();
+    userInput = $("input").val();
+    api = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + userInput + "&format=json";
+    $.ajax({
+      url: api,
+      dataType: "jsonp",
+      // jsonp: "jsoncallback",
+      success:
+      function processJson(json){
+        for (var i = 0; i < json.query.search.length; i++){
+          var title = json.query.search[i].title;
+          var snippet = json.query.search[i].snippet;
+          var urlLink = "https://en.wikipedia.org/?curid=" + json.query.search[i].pageid;
+          $("#results").append(
+            "<a target='_blank' href=" + urlLink + ">" +
+              "<div>" +
+                "<span id='title'>" + title + "</span><br>" +
+                "<span id='snippet'>" + snippet + "</span>" +
+              "</div>" +
+            "</a>"
+          );
+        }//end for
+      },//end processJson
+      error:
+      function(XMLHttpRequest, textStatus, errorThrown) {
+        alert("Status: " + textStatus); alert("Error: " + errorThrown);
+      }
+    });//end .ajax
+  }//end if
+
+  var inputVal = $("input").val();
+  console.log(inputVal);
+  if (inputVal == "") {
+    $("#results").empty();
+  }
+});//end onkeydown
+
+//implement custom infinite scroll
