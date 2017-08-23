87f1e86347bd4e5b8f3f9fe3a6625ea05929638f
Kent Saeteurn <kentsaeteurn@gmail.com>
2017-08-22 21:58:35 -0700

    fix typeerror issue using hasOwnProperty to test if "thumbnail" key exists for result

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-var api, userInput;
+var api, userInput, img, newResImg, origImgWidth;
 $("input").on("keyup", function(event) {
   if (event.which == 13) {
     $("#results").empty();
     userInput = $("input").val();
+    //add "&gsroffset=num" to get next num pages in search result
+    //get those results and append to results area to do custom infinite scroll
     api = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts|pageimages&exintro&exsentences=2&generator=search&gsrsearch=" + userInput + "&format=json";
     $.ajax({
       url: api,
@@ -11,11 +13,13 @@ $("input").on("keyup", function(event) {
       success:
       function processJson(json){
         var pages = json.query.pages;
-        var pageIdKeys = Object.keys(pages)
+        var pageIdKeys = Object.keys(pages);
         for (var i = 0; i < pageIdKeys.length; i++){
-          var origImgWidth = json.query.pages[pageIdKeys[i]].thumbnail.width;
-          var img = json.query.pages[pageIdKeys[i]].thumbnail.source;
-          var newResImg = img.replace(origImgWidth, "100");
+          if (json.query.pages[pageIdKeys[i]].hasOwnProperty('thumbnail')) {
+            origImgWidth = json.query.pages[pageIdKeys[i]].thumbnail.width;
+            img = json.query.pages[pageIdKeys[i]].thumbnail.source;
+            newResImg = img.replace(origImgWidth, "100");
+          }
           var title = json.query.pages[pageIdKeys[i]].title;
           var extract = json.query.pages[pageIdKeys[i]].extract;
           var urlLink = "https://en.wikipedia.org/?curid=" + json.query.pages[pageIdKeys[i]].pageid;
@@ -23,7 +27,7 @@ $("input").on("keyup", function(event) {
             "<a target='_blank' href=" + urlLink + ">" +
               "<div>" +
                 "<span id='title'>" + title + "</span><br>" +
-                "<img src=" + newResImg + "><br>" +
+                (json.query.pages[pageIdKeys[i]].hasOwnProperty('thumbnail') ? "<img src=" + newResImg + "><br>" : "") +
                 "<span id='snippet'>" + extract + "</span>" +
               "</div>" +
             "</a>"
@@ -44,3 +48,12 @@ $("input").on("keyup", function(event) {
 });//end onkeydown
 
 //implement custom infinite scroll
+//see note at the very top
+
+
+
+/*******/
+/*NOTES*/
+/*******/
+//hasOwnProperty used to cover cases where result doesn't have thumbnail;
+//without it, we'll get a typeerror and the rest of the results will not append to the dom
